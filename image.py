bc5221c3d6466d19127ff7ab1ce4834c718df931
Tong Xie <txie@Tongs-MacBook-Pro.local>
2022-12-03 18:07:07 -0800

    Added an image processing class, updated README

--- image.py
+++ image.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import sys
 
 class image_process:
     def __init__(self, df):
@@ -11,3 +12,17 @@ class image_process:
         fig, ax = plt.subplots(1)
         img = self.df.iloc[n].values.reshape(28, 28)
         ax.imshow(img, cmap = plt.cm.binary)
+
+    def show_nine_image (self, n):
+        '''
+            returns 9 images starting from the nth index in dataset on a 3x3 grid
+        '''
+        fig, ax = plt.subplots(3,3, figsize=(8,8))
+        for i in range(3):
+            for j in range (3):
+                try:       
+                    img = self.df.iloc[n+3*i+j].values.reshape(28,28)
+                    ax[i][j].imshow(img, cmap = plt.cm.binary)
+                except IndexError as e:
+                    print('Too big! Not enought iamges.', file=sys.stderr)
+                    return
