7aa69da54b2fc2eec4fbfb77386e406e13794d91
haoyu li <haoyul1@vmware.com>
2022-12-11 01:42:15 -0800

    Add comments, docstring, explanations to code and graphs

--- logistic.py
+++ logistic.py
@@ -1,32 +1,65 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# To be honest, I think this class should be seperated into individual functions
 class binary_logistic:
     def __init__(self, X, y):
         self.X = X
         self.y = y
     
     def gradient_descent_logistic(self, X, y, W0 = None, iter_times=100, stopping_diff=0.01):
+        """ Binary logistic regression using gradient descent
+
+        Args:
+            X (p * n list like object): Sample data, for our usage is 2d array where each column is pixels for a number
+            y (n * 1 list): Label for each number
+            W0 (_type_, optional): Starting parameters. Defaults to None.
+            iter_times (int, optional): Max number of iterations. Defaults to 100.
+            stopping_diff (float, optional): Error to be considered as converged. Defaults to 0.01.
+
+        Returns:
+            list of floats: Fitted parameters
+        """
+        # If no starting parameters given, just choose randomly
         if W0 is None:
             W0 = np.random.rand(X.shape[0], 1)
         
         W1 = W0.copy()
         i = 0
         grad = np.ones(W0.shape)
+        # while not converged or reached maximum iteration
         while (i < iter_times) and np.linalg.norm(grad) > stopping_diff:
+            # @ is matrix multiplication
             Q = 1 / (1 + np.exp(-X.T @ W1))
             grad = X @ (Q - y)
+            # The (np.log(i+1) / (((i+1) ** (0.5)))) thing is the hyperparameter
+            # I have no idea why somehow this number is often chosen
             W1 = W1 - (np.log(i+1) / (((i+1) ** (0.5)))) * grad
             i = i + 1
         return W1
     
     def compute_accuracy(self, X_test, y_test, W, relation={0:0, 1:1}, threshold=0.5):
+        """ Compute the accuracy of of trained paramters in test set
+
+        Args:
+            X_test (n * p matrix): Test set pixels
+            y_test (n * 1 matrix): labels for test set
+            W (list with length n): Fitted parameters
+            relation (dict, optional): {x:0, y:1} if we would like to classify between number x and y . Defaults to {0:0, 1:1}.
+            threshold (float, optional): Probability bigger than this threshold will be treated as 1. Defaults to 0.5.
+
+        Returns:
+            float: percentage of accurate prediction
+        """
         count = 0
         for i in range(len(X_test)):
             predict = -1
             score = 0
+            # This calculates the probability that the given image represent with label 0 
+            # based on the fitted parameters
             for j in range(len(X_test[0])):
                 score += X_test[i,j] * W[j, 0]
+                
             if score > threshold:
                 predict = 1
             else:
@@ -37,12 +70,32 @@ class binary_logistic:
         return count / len(y_test)
     
     def extract_two_numbers(self, i, j):
+        """ Extract all data from dataset that related to number i and number j
+
+        Args:
+            i (int): First number
+            j (int): Second number
+
+        Returns:
+            Dataframe, Dataframe: Pixels for all numbers with label i and j, Correponding labels
+        """
+        # Raise ValueError for incorrect numbers
         if not (0 <= i and i <= 9) or not (0 <= j and j <= 9):
-            return
+            raise ValueError
         boolean_index = (self.y['class'] == i) | (self.y['class'] == j)
         return self.X[boolean_index], self.y[boolean_index]
     
     def train(self, i, j, testsize=0.3):
+        """ Essentially a wrapper for the whole class
+
+        Args:
+            i (int): number to classify
+            j (int): the other number to classify
+            testsize (float, optional): Percentage for the test size. Defaults to 0.3.
+
+        Returns:
+            List[float], float: fitted parameters, percentage of correctness in test set
+        """
         X_i_j, y_i_j = self.extract_two_numbers(i, j)
         X_i_j_train, X_i_j_test, y_i_j_train, y_i_j_test = train_test_split(X_i_j, y_i_j, test_size = testsize)
         X_i_j_train_after = X_i_j_train.values
@@ -59,7 +112,14 @@ class binary_logistic:
         return W, score
     
     def compute_all_accuracy(self):
+        """ Wrapper for trying to classify between every pair of numbers
+
+        Returns:
+            List[List[float]], size: 10 * 10: Matrix with entry i, j represents the training score for classifying number i and j
+        """
         result = np.reshape(np.arange(100, dtype=float), (10, 10))
+        
+        # Just compute halfway, the matrix should be symmetric
         for i in range(10):
             for j in range(i, 10):
                 if i == j:
@@ -67,11 +127,15 @@ class binary_logistic:
                 else:
                     W, score = self.train(i, j)
                     result[i,j] = score
+        
+        # Flip the matrix to make it symmetric           
         for i in range(10):
             for j in range(10):
                 if j >= i:
                     continue
                 else:
                     result[i, j] = result[j, i]
+                    
+        # So that we don't have that many digits...
         return np.round(result, 4)
         
--- read_data.py
+++ read_data.py
@@ -4,18 +4,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
 
 def read_from_openml():
+    """ Get dataset from openml MNIST
+
+    Returns:
+        pandas dataframe containing pixels of numbers and thier labels
+    """
     X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
     X = X / 255
     return X,y
 
 def export_csv():
+    """ Export the panda dataframe to .csv file so that 
+        subsequent fetch data becomes faster
+    """
     X,y = read_from_openml()
+    # Store the file in the current working directory
     path_data = os.getcwd() + '/data.csv'
     path_result = os.getcwd() + '/result.csv'
+    # Index equals False helps to eliminate the additional
+    # index column when actually exporting to panda dataframe.
     X.to_csv(path_data, index=False)
     y.to_csv(path_result, index=False)
 
 def read_from_csv():
+    """ Read data from .csv file
+        This only works if those csv files
+        generated in the previous function stores in
+        the current directory
+
+    Returns:
+        pandas dataframe containing pixels of numbers and thier labels
+    """
     path_data = os.getcwd() + '/data.csv'
     path_result = os.getcwd() + '/result.csv'
     X = pd.read_csv(path_data)
@@ -23,13 +42,14 @@ def read_from_csv():
     return X,y
 
 def read_data():
+    """ Read from csv if possible, else read from openml
+
+    Returns:
+        pandas dataframe containing pixels of numbers and thier labels
+    """
     try:
         X, y = read_from_csv()
         return X,y
     except FileNotFoundError:
         export_csv()
-    # if not os.path.isfile(os.getcwd() + '/data.csv'):
-    #     export_csv()
-    # X, y = read_from_csv()
-    # return X,y
     
